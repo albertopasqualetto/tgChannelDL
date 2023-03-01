f8c91b77332dcdb49fd8411b386562993a801aa3
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2023-03-01 20:39:33 +0100

    Save downloads in a folder

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 from dotenv import load_dotenv
 from telethon.sync import TelegramClient
 # maybe move to async version
+# maybe download to user's downloads folder https://stackoverflow.com/a/48706260/12506990
 
 load_dotenv()
 
@@ -23,7 +24,7 @@ def download_from_chat(client, chat_id):
         pbar = tqdm(total=m.file.size, unit='B', unit_scale=True, unit_divisor=1024, desc="Downloading")
         filename = m.file.name if m.file.name else m.file.id
         pbar.set_description("Downloading " + filename)
-        client.download_media(m, progress_callback=callback)    # TODO save to folder
+        client.download_media(m, progress_callback=callback, file="./downloads/"+filename)
         pbar.close()
 
 
@@ -31,6 +32,7 @@ if __name__ == '__main__':
     client = TelegramClient('tgChannelDL', int(os.environ.get('API_ID')), os.environ.get('API_HASH'))
     client.start(os.environ.get('PHONE_NUMBER'), os.environ.get('TWOFA_PASSWORD'))
 
+    print("Download all files from specified channel")
     download_from_chat(client, os.environ.get('CHANNEL_INVITE_LINK'))
-
+    print("Done!")
     client.disconnect()
