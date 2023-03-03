4ee17cc4cb4c6b2012eeb7b3c041e0b470224f74
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2023-03-03 14:29:35 +0100

    Resume or restart

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ from telethon.sync import TelegramClient
 
 load_dotenv()
 
-# if resuming download from a certain point, set manually argv[1]=old_id to the last downloaded file id
+# argv[1] = "resume" or "restart" if resuming download after deleting files from Telegram
+# if resuming download from a certain point, set manually argv[2]=old_id to the last downloaded file number
 old_id = 0
 if len(sys.argv) > 1:
-    old_id = int(sys.argv[1])
+    old_id = int(sys.argv[2])
 
 
 def callback(current, total):
@@ -23,14 +24,19 @@ def download_from_chat(client, chat_id):
     messages = client.get_messages(entity=chat_id)
     messages.reverse()  # Download from oldest to newest
     for idm, m in enumerate(messages):  # idm[0] is always the channel creation message
-        if idm < old_id:    # skip already downloaded files
+        if len(sys.argv) > 1 and sys.argv[1] == "resume" and idm < old_id:    # skip already downloaded files but re-download the last one (probably corrupted)
             continue
         if m.action or not m.file:  # skip messages without files
             continue
 
+        if len(sys.argv) > 1 and sys.argv[1] == "resume":
+            number = idm
+        elif len(sys.argv) > 1 and sys.argv[1] == "restart":
+            number = idm + old_id
+
         global pbar
         pbar = tqdm(total=m.file.size, unit='B', unit_scale=True, unit_divisor=1024, desc="Downloading")
-        filename = m.file.name if m.file.name else m.file.id+" ({})".format(idm+old_id)   # cannot download images
+        filename = m.file.name if m.file.name else m.file.id+" ({})".format(number)   # cannot download images
         pbar.set_description("Downloading " + filename)
         client.download_media(m, progress_callback=callback, file="./downloads/"+filename)
         pbar.close()
